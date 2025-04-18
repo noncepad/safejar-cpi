6b15503283bb2d103d0398e4dd7798da14fbaae3
Joel De Jesus <joel@noncepad.com>
2025-04-18 14:29:47 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#[cfg(feature = "all")]
 anchor_gen::generate_cpi_crate!("idl.json");
 
+#[cfg(feature = "all")]
 declare_id!("TRSY7YgS3tcDoi6ZgTp2MmPJpXHyCVrGaFhL7HLdQc9");
+
+pub mod simple;
---
+++ src/simple.rs
@@ -0,0 +1,15 @@
+pub fn controller_discriminator() -> [u8; 8] {
+    [184, 79, 171, 0, 183, 43, 113, 110]
+}
+
+pub fn delegation_discriminator() -> [u8; 8] {
+    [237, 90, 140, 159, 124, 255, 243, 80]
+}
+
+pub fn ruleaccumulator_discriminator() -> [u8; 8] {
+    [127, 132, 189, 170, 68, 38, 206, 135]
+}
+
+pub fn spendrequest_discriminator() -> [u8; 8] {
+    [71, 251, 215, 71, 98, 153, 90, 25]
+}
